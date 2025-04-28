0046199ef812a90658d9ceb57168272c2330f021
Yamila Silva <silvayamila84@gmail.com>
2025-04-28 09:38:56 -0300

    configuracion de librerias externa y estructura del proyecto

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"sales-api/api"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
+	r := gin.Default()
+
+	// Inicializar las rutas
+	api.InitRoutes(r)
+
+	// Iniciar el servidor en el puerto 8080
+	r.Run(":8081")
 
 }
